fd7da85abc58d721f0f0badf11d852c79af5badc
ziadkhalifaa <sayedibrahim5500@gmail.com>
2026-08-28 05:35:48 +0300

    fix: make exercises_json migration idempotent

--- server/src/migrations/038_exercise_plan_version_exercises_json.js
+++ server/src/migrations/038_exercise_plan_version_exercises_json.js
@@ -1,12 +1,18 @@
 import { DataTypes } from "sequelize";
 
 export async function up(queryInterface) {
-  await queryInterface.addColumn("exercise_plan_version", "exercises_json", {
-    type: DataTypes.JSON,
-    allowNull: true,
-  });
+  const table = await queryInterface.describeTable("exercise_plan_version");
+  if (!table.exercises_json) {
+    await queryInterface.addColumn("exercise_plan_version", "exercises_json", {
+      type: DataTypes.JSON,
+      allowNull: true,
+    });
+  }
 }
 
 export async function down(queryInterface) {
-  await queryInterface.removeColumn("exercise_plan_version", "exercises_json");
+  const table = await queryInterface.describeTable("exercise_plan_version");
+  if (table.exercises_json) {
+    await queryInterface.removeColumn("exercise_plan_version", "exercises_json");
+  }
 }
