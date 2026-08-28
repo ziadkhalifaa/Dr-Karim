066e8b0a4ba20da7b6e92a0d343fdf4ade2d9b31
ziadkhalifaa <sayedibrahim5500@gmail.com>
2026-08-28 05:24:58 +0300

    fix: use DataTypes.JSON in migration

--- server/src/migrations/038_exercise_plan_version_exercises_json.js
+++ server/src/migrations/038_exercise_plan_version_exercises_json.js
@@ -1,10 +1,12 @@
-export async function up(queryInterface, Sequelize) {
+import { DataTypes } from "sequelize";
+
+export async function up(queryInterface) {
   await queryInterface.addColumn("exercise_plan_version", "exercises_json", {
-    type: Sequelize.JSON,
+    type: DataTypes.JSON,
     allowNull: true,
   });
 }
 
-export async function down(queryInterface, Sequelize) {
+export async function down(queryInterface) {
   await queryInterface.removeColumn("exercise_plan_version", "exercises_json");
 }
